6eda17eb682a6211f32b05ef23c82cf3b801a9e8
emaelia <emaelia.norman@gmail.com>
2020-04-15 11:27:30 +0200

    Continued problems with access to firebase, case sensitivity issue possibly resolved

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid';
-import database from '../firebase/firebase';
+import database from '../Firebase/firebase';
 
 //ADD_EXPENSE action generator
 export const addExpense = (expense) => ({
